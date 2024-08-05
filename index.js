946d2fd010c6e6046209dfa8dc1c524d9920a70a
n1ghtcrawller <borisgr@bk.ru>
2024-08-06 02:51:33 +0300

    Modified port

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/web-data', async (req, res) => {
       id: queryId,
       title: 'Успешная покупка',
       input_message_content: {
-        message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+        message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}}`
       }
     });
     return res.status(200).json({});
