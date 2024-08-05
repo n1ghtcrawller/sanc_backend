30264af172566073e1d894a50c7fd3e35adc43a3
n1ghtcrawller <borisgr@bk.ru>
2024-08-06 02:55:34 +0300

    Modified port

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-  console.log('Received data:', req.body); // Логируем входящие данные
   const {queryId, products = [], totalPrice} = req.body;
   try {
     await bot.answerWebAppQuery(queryId, {
@@ -60,15 +59,14 @@ app.post('/web-data', async (req, res) => {
       id: queryId,
       title: 'Успешная покупка',
       input_message_content: {
-        message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}}`
+        message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
       }
-    });
+    })
     return res.status(200).json({});
   } catch (e) {
-    console.error('Error:', e); // Логируем ошибку
-    return res.status(500).json({});
+    return res.status(500).json({})
   }
-});
+})
 
 const PORT = 8000;
 
