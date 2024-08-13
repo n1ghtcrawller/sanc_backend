0722fc026ee3fb79b9357d745f7d8c011f124918
n1ghtcrawller <borisgr@bk.ru>
2024-08-13 21:35:20 +0300

    Bug Fixing for backend

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/web-data', async (req, res) => {
 
   try {
     // Формируем сообщение с товарами и их количеством
-    const productList = products.map(item => ` - ${item.title}, размер ${item.size} (Количество: ${item.count})\n`);
+    const productList = products.map(item => `${item.title}, размер ${item.size} (Количество: ${item.count})\n`);
 
     // Отправляем инвойс
     await bot.sendInvoice(
